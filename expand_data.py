fac660af4411a1be4b6978509f3aa5b52a26fe4d
柴华君 <chaihj15@mails.tsinghua.edu.cn>
2018-06-03 20:27:25 +0800

    fix bugs

--- expand_data.py
+++ expand_data.py
@@ -3,32 +3,30 @@ import numpy as np
 from PIL import Image
 import os  
 
-def rot_sym(path, file):
+def rot_sym(path, filename):
     angle = [90, 180, 270]
-    img = Image.open(path+"/"+file); #打开文件
-    img.transpose(Image.FLIP_LEFT_RIGHT).save(path + '/' + 'sym_' + file)  
+    img = Image.open(path + '\\' + filename); #打开文件
+    img.transpose(Image.FLIP_LEFT_RIGHT).save(path + '\\' + 'sym_' + filename)  
     for ang in angle:
-        img.rotate(ang).save(path + '/' + 'rot_' + str(ang) + '_' + file)
-        img.rotate(ang).transpose(Image.FLIP_LEFT_RIGHT).save(path + '/' + 'sym_' + str(ang) + '_' + file)
+        img.rotate(ang).save(path + '\\' + 'rot_' + str(ang) + '_' + filename)
+        img.rotate(ang).transpose(Image.FLIP_LEFT_RIGHT).save(path + '\\' + 'sym_' + str(ang) + '_' + filename)
 
 def expand(path):
     files = os.listdir(path) #得到文件夹下的所有文件名称  
-    for file in files: #遍历文件夹  
-        # if not os.path.isdir(file): #判断是否是文件夹，不是文件夹才打开  
-        #     rot_sym(path, file)
-        # else:
-        sub_path = path + '/' + file 
-        sub_files = os.listdir(sub_path)
-        for img in sub_files:  #training含多个子文件夹
-            rot_sym(sub_path, img)
-                         
+    for f in files: #遍历文件夹  
+        tmp_path = os.path.join(path, f)
+        if not os.path.isdir(tmp_path): #判断是否是文件夹，不是文件夹才打开  
+            print('its not dir ' + f)
+            rot_sym(path, f)
+        else:
+            expand(tmp_path)                        
 
 def img_convert(path):
     files = os.listdir(path) #得到文件夹下的所有文件名称
     width = 277
     height = 277
-     for file in files: #遍历文件夹  
-        sub_path = path + '/' + file 
+    for file in files: #遍历文件夹  
+        sub_path = path + '\\' + file 
         sub_files = os.listdir(sub_path)
         for img in sub_files:  #training含多个子文件夹
             img = img.resize((width, height))  
